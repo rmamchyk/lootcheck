def2f4fe9a1163473312e4887b18c2375149fec3
rmamchyk <roman.mamchyk@gmail.com>
2018-12-23 19:35:43 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
+import App from './components/App';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'; 
+import balanceReducer from './balance';
+import bitcoinReducer from './bitcoin';
+
+export default combineReducers({
+    balance: balanceReducer,
+    bitcoin: bitcoinReducer
+});
